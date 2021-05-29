b356a3764af2e27aded885b3453747744443b950
Sivaram D <siv2ram@gmail.com>
2021-05-29 14:59:05 +0530

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -2,6 +2,6 @@
  sudo apt-get update --fix-missing
  sudo apt-get install locales
  locale-gen en_US.UTF-8
- export LC_CTYPE='en_US.UTF8'
- export LANG='en_US.UTF8' 
+ export LC_CTYPE='en_US.UTF-8'
+ export LANG='en_US.UTF-8' 
  pip install git+https://github.com/LIAAD/yake 
