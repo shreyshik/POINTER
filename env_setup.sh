10a7c3869a752b614852959368cd0776ca0e741c
siv2r <siv2ram@gmail.com>
2021-05-29 14:53:54 +0530

    first working configuration

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,4 @@
-conda create --prefix ./pointer_env python=3.6
+conda create -n pointer_env python=3.6
 conda activate pointer_env
 conda install pytorch
 pip install tqdm
@@ -7,4 +7,5 @@ pip install requests
 pip install regex
 pip install nltk
 bash requirement.sh
+pip install sapcy
 python -m spacy download en_core_web_sm
--- requirement.sh
+++ requirement.sh
@@ -1,7 +1,7 @@
  pip install user joblib
- aptget update fixmissing
- aptget install y locales
- localegen en_US.UTF8
+ sudo apt-get update --fix-missing
+ sudo apt-get install locales
+ locale-gen en_US.UTF-8
  export LC_CTYPE='en_US.UTF8'
  export LANG='en_US.UTF8' 
  pip install git+https://github.com/LIAAD/yake 
